b0d18bafa46be041818d65e7a3ae09586791131e
maxwellmandina <maxwell.mandina@gmail.com>
2025-02-28 13:47:48 -0500

    feat: reintroduce jit, make process_batches have no side_effects

--- linear_regression_jax/mini_batches.py
+++ linear_regression_jax/mini_batches.py
@@ -20,14 +20,13 @@ def update(params, X, y):
     new_params = (params[0] - ALPHA * grads[0],
                   params[1] - ALPHA * grads[1])
     return new_params
-
-def process_batchs(inputs, true_outputs, outputs):
-    # Loop over mini-batches and update parameters using the update function
+@jit
+def process_batches(inputs, true_outputs, m, b):
     for idx in range(0, len(true_outputs), BATCH_SIZE):
         X_batch = inputs[idx:idx+BATCH_SIZE]
         y_batch = true_outputs[idx:idx+BATCH_SIZE]
-        new_params = update((outputs['m'], outputs['b']), X_batch, y_batch)
-        outputs['m'], outputs['b'] = new_params
+        m, b = update((m, b), X_batch, y_batch)
+    return m, b
 
 def schotastic_linear_regression():
     # Start 
@@ -37,7 +36,6 @@ def schotastic_linear_regression():
     true_outputs = 12 * inputs - 3
     m = np.random.randn()
     b = np.random.randn()
-    outputs = {'mse': float('inf'), 'm': m, 'b': b}
     
     loss_history = []
  
@@ -47,13 +45,14 @@ def schotastic_linear_regression():
         inputs = inputs[indices]
         true_outputs = true_outputs[indices]
         # Compute predictions and loss
-        outputs['mse'] = jax_mse(outputs['m'], outputs['b'], inputs, true_outputs)
-        loss_history.append(float(outputs['mse']))
-        # Update parameters with mini-batch processing
-        process_batchs(inputs, true_outputs, outputs)
+        outputs_mse = jax_mse(m, b, inputs, true_outputs)
+        loss_history.append(float(outputs_mse))
+
+    # Update parameters with mini-batch processing
+        m, b = process_batches(inputs, true_outputs, m, b)
     
     elapsed_time = time.time() - start_time
-    print(f"After {EPOCHS} epochs: Loss = {outputs['mse']}, m = {outputs['m']}, b = {outputs['b']}")
+    print(f"After {EPOCHS} epochs: Loss = {outputs_mse}, m = {m}, b = {b}")
     print(f"Execution time: {elapsed_time:.4f} seconds")
     
     plt.figure(figsize=(8, 5))
